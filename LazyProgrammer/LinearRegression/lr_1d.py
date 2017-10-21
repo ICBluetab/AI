fa49716d8a97de94b5f6561be385d805256aa394
Iñigo Crespo <inigo.crespo@mail.com>
2017-10-21 13:11:56 +0200

    LinearRegression r square

--- LazyProgrammer/LinearRegression/lr_1d.py
+++ LazyProgrammer/LinearRegression/lr_1d.py
@@ -17,6 +17,17 @@ b = (Y.mean() * X.dot(X) - X.mean() * X.dot(Y)) / denominator
 
 Yhat = a * X + b
 
+RR = Y - Yhat
+RR = np.power(RR, 2)
+RR = RR.dot(RR)
+RT = Y - Y.mean()
+RT = np.power(RT, 2)
+RT = RT.dot(RT)
+
+R2 = 1 - RR/RT
+
+print("R2 " + str(R2))
+
 plt.scatter(X, Y)
 plt.plot(X, Yhat)
 plt.show()
